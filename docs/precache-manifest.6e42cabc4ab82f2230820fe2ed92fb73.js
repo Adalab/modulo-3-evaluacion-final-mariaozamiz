f57046bc79d7005afd217fbfab99275f2d1ffde2
Maria Ozamiz <ozamiz.m@gmail.com>
2020-08-10 11:51:38 +0200

    run docs

---
+++ docs/precache-manifest.6e42cabc4ab82f2230820fe2ed92fb73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7e598351bc8658ee4fbf96699f180d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8dfde5d431cdbfd266e",
+    "url": "./static/css/main.c3d16853.chunk.css"
+  },
+  {
+    "revision": "7a5ab07e59817f08433e",
+    "url": "./static/js/2.f21ac4d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f21ac4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dfde5d431cdbfd266e",
+    "url": "./static/js/main.9a2d542c.chunk.js"
+  },
+  {
+    "revision": "e21397ecf58f77891282",
+    "url": "./static/js/runtime-main.8ee73f49.js"
+  },
+  {
+    "revision": "ee3b81c90bdc8c4fb29153a0ccbe0977",
+    "url": "./static/media/heartbeat-solid.ee3b81c9.svg"
+  },
+  {
+    "revision": "4485f8865ab57c59cc2d7555eb8254a2",
+    "url": "./static/media/question-circle-solid.4485f886.svg"
+  },
+  {
+    "revision": "422193e57361fb677340788992e64a27",
+    "url": "./static/media/reddit-alien-brands.422193e5.svg"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/rick-and-morty-logo.75509f2e.png"
+  },
+  {
+    "revision": "63c8b224de7461baae1f47576a460de3",
+    "url": "./static/media/skull-solid.63c8b224.svg"
+  },
+  {
+    "revision": "c35809137a056d631f4d908dac3334f3",
+    "url": "./static/media/user-circle-solid.c3580913.svg"
+  },
+  {
+    "revision": "b1f2a76280a27ad7ff98924882c41e78",
+    "url": "./static/media/window-close-solid.b1f2a762.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86302f544b7e3dc334b91c9eb5806a9b.js"
+  "./precache-manifest.6e42cabc4ab82f2230820fe2ed92fb73.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
